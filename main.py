9f5ee2daedbbf1e6dd58fcba5261fb262badab1e
Tagptroll1 <thomas@galehuset.org>
2018-05-28 19:29:22 +0200

    init

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import Ridge
+from sklearn import tree
 from sklearn.neighbors import KNeighborsRegressor
+
 import numpy as np
 
 n_level = {
@@ -42,9 +44,11 @@ for line in csv_lines:
 
 nparray = np.array(array)
 
-X = []
-y = []
 
+
+X_binary = []
+X = list()
+y = []
 for entry in nparray[1:]:
     n_level_entry = n_level[entry[0]]
     species_entry = species[entry[1]]
@@ -52,16 +56,47 @@ for entry in nparray[1:]:
     _A, _B, _C, _D, _E, _F = get_binary_species(species_entry)
 
     weight = float(entry[2])
-    X.append([_L, _M, _H, _A, _B, _C, _D, _E, _F])
+    X.append([n_level_entry, species_entry])
+    X_binary.append([_L, _M, _H, _A, _B, _C, _D, _E, _F])
     y.append(weight)
 
-X = np.array(X)
-y = np.array(y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+X_binary = np.array(X_binary)
+y = np.array(y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_binary, y, test_size=0.3)
+
+Xx_train, Xx_test, yy_train, yy_test = train_test_split(X, y, test_size=0.3)
 #print("X_train",X_train, "y_train", y_train)
 #print("X_test",X_test, "y_test", y_test)
+
+
 ## Testing
+tree_param = {
+    "criterion": "mse", 
+    "splitter": "best", 
+    "max_depth": None, 
+    "min_samples_split": 3, 
+    "min_samples_leaf": 1,
+    "min_weight_fraction_leaf": 0.0, 
+    "max_features": None, 
+    "random_state": None, 
+    "max_leaf_nodes": None, 
+    "min_impurity_decrease": 0.0, 
+    "min_impurity_split": None, 
+    "presort": False
+}
+tree1 = tree.DecisionTreeRegressor(**tree_param)
+tree1 = tree1.fit(X_train, y_train)
+tree_score = tree1.score(X_test, y_test)
+print(f"Tree score: {tree_score:.2f}")
+
+tree2 = tree.DecisionTreeRegressor(**tree_param)
+tree2 = tree2.fit(Xx_train, yy_train)
+tree_score2 = tree2.score(Xx_test, yy_test)
+print(f"Tree2 score: {tree_score2:.2f}")
+
+
 linearRegression_param = {
     "fit_intercept": True,
     "normalize": False,
@@ -69,9 +104,9 @@ linearRegression_param = {
     "n_jobs": 1
 }
 lr = linear_model.LinearRegression(**linearRegression_param)
-lr.fit(X_train, y_train)
+lr = lr.fit(X_train, y_train)
 score = lr.score(X_test, y_test)
-print("Score",f"{score:.2f}")
+print(f"Score: {score:.2f}")
 
 knn_param = {
     "n_neighbors": 5, 
@@ -84,7 +119,7 @@ knn_param = {
     "n_jobs": 1
 }
 knn = KNeighborsRegressor(**knn_param)
-knn.fit(X_train, y_train)
+knn = knn.fit(X_train, y_train)
 score_knn = knn.score(X_test, y_test)
 print("knn score:", f"{score_knn:.2f}")
 
@@ -99,7 +134,7 @@ ridge_param = {
     "random_state": None
 }
 rid = Ridge(**ridge_param)
-rid.fit(X_train, y_train)
+rid = rid.fit(X_train, y_train)
 score_rid = rid.score(X_test, y_test)
 print("ridge score:", f"{score_rid:.2f}")
 
